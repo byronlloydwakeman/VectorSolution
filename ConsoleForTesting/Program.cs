5d0b2f3527a524407eb5113d8a35a1742afc58bc
Byron Lloyd-Wakeman <byzstorm1103@gmail.com>
2021-04-09 11:56:28 -0800

    Add project files.

---
+++ ConsoleForTesting/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using VectorLibrary;
+
+namespace ConsoleForTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector vector1 = new Vector(1, 2);
+            Vector vector2 = new Vector(2, 5);
+
+            Console.WriteLine($"Dot product of {vector1} and {vector2} is {Vector.DotProduct(vector1, vector2)}");
+
+            Console.WriteLine(vector1.Magnitude());
+
+            Console.WriteLine(Vector.FindAngleBetweenVectors(vector1, vector2));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ VectorLibrary/Vector.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorLibrary
+{
+    public class Vector
+    {
+        List<int> DataValues = new List<int>();
+        public int Dimension { get; }
+
+        public Vector(int x, int y)
+        {
+            Dimension = 2;
+            DataValues.Add(x);
+            DataValues.Add(y);
+        }
+
+        public Vector(int x, int y, int z)
+        {
+            Dimension = 3;
+            DataValues.Add(x);
+            DataValues.Add(y);
+            DataValues.Add(z);
+        }
+
+        private void IsIndexInRange(int axis)
+        {
+            if (axis > Dimension)
+            {
+                throw new ArgumentException("Invalid dimension argument");
+            }
+            if(axis < 1)
+            {
+                throw new ArgumentException("Dimension parameter cannot be less than 1");
+            }
+        }
+
+        public void EditVector(int axis, int newValue)
+        {
+            //Make sure the index is within range
+            IsIndexInRange(axis);
+
+            //Change value at given index
+            DataValues[axis - 1] = newValue;
+        }
+
+
+        public double Magnitude()
+        {
+            double returnSum = 0;
+            foreach (var element in DataValues)
+            {
+                returnSum += Math.Pow(Convert.ToDouble(element), 2.0);
+            }
+
+            return Math.Pow(returnSum, 0.5);
+        }
+
+
+        private static void AreVectorsOfSameDimension(int d1, int d2)
+        {
+            if (d1 != d2)
+            {
+                throw new ArgumentException("Dimension of given vectors aren't equal");
+            }
+        }
+
+        public static int DotProduct(Vector v1, Vector v2)
+        {
+            //Make sure dot product can be performed
+            AreVectorsOfSameDimension(v1.Dimension, v2.Dimension);
+
+            int tempSum = 0;
+            for (int i = 0; i < v1.DataValues.Count; i++)
+            {
+                tempSum += v1.DataValues[i] * v2.DataValues[i];
+            }
+
+            return tempSum;
+        }
+
+        /// <summary>
+        /// Returns the angle between two vectors in radians
+        /// </summary>
+        public static double FindAngleBetweenVectors(Vector v1, Vector v2)
+        {
+            return Math.Acos(DotProduct(v1, v2) / (v1.Magnitude() * v2.Magnitude()));
+        }
+
+        public override string ToString()
+        {
+            string returnString = "";
+            foreach (var element in DataValues)
+            {
+                returnString += "\n" + element.ToString() ;
+            }
+
+            returnString += "\n";
+
+            return returnString;
+        }
+    }
+}
