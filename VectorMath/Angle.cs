bf06dc385f9785a0e608ddfc5c0ed78cd7872a90
Byron Lloyd-Wakeman <byzstorm1103@gmail.com>
2021-04-12 12:05:07 -0800

    Add VectorMath Project Add Angle Class

---
+++ VectorMath/Angle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VectorLibrary;
+
+namespace VectorMath
+{
+    public static class Angle
+    {
+        public static double FindAngleBetweenLines(ILine line1, ILine line2)
+        {
+            return Vector.FindAngleBetweenVectors(line1.ReturnDirectionVector(), line2.ReturnDirectionVector());
+        }
+
+        public static double FindAngleBetweenPlanes(IPlane plane1, IPlane plane2)
+        {
+            return Vector.FindAngleBetweenVectors(plane1.ReturnNormalVector(), plane2.ReturnNormalVector());
+        }
+    }
+}
