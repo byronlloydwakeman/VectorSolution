8ec712cd25c11a310fbd33f71b6f2bac5ebf0288
Byron Lloyd-Wakeman <byzstorm1103@gmail.com>
2021-04-12 12:12:58 -0800

    Add ScalarPlane Class
    Added ScalarPlane Class which includes some methods and properies

---
+++ VectorLibrary/ScalarPlane.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorLibrary
+{
+    public class ScalarPlane : IPlane
+    {
+        public Vector NormalVector { get; set; }
+        public Vector GeneralVector { get; set; }
+        public int Scalar { get; set; }
+
+        public ScalarPlane(Vector NormalVector, int Scalar)
+        {
+            this.NormalVector = NormalVector;
+            this.Scalar = Scalar;
+        }
+
+        public Vector ReturnNormalVector()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return $"r . {NormalVector}";
+        }
+    }
+}
